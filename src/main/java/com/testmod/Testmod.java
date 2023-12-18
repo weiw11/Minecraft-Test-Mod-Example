028cad15fbd2a4c7d8f932c9c24dfd50a0bf41b4
Wei Wang <62213594+weiw11@users.noreply.github.com>
2023-12-18 16:14:41 -0500

    first commit for example minecraft fabric mod

---
+++ src/main/java/com/testmod/Testmod.java
@@ -0,0 +1,34 @@
+package com.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Testmod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("testmod");
+
+	public static final Block EXAMPLE_BLOCK = new ExampleBlock(FabricBlockSettings.create().strength(4.0f).requiresTool());
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.BLOCK, new Identifier("testmod", "example_block"), EXAMPLE_BLOCK);
+		Registry.register(Registries.ITEM, new Identifier("testmod", "example_block"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings()));
+
+	}
+}
